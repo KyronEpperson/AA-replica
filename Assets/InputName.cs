f6abada66af877c7c00f1a07c9a305b2fe579492
KyronEpperson <epperson.kyron@gmail.com>
2021-03-14 04:11:47 -0500

    Input field fixed
    nice

---
+++ Assets/InputName.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputName : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void storeName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = theName;
+    }
+
+}
